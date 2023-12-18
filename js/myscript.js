98741fada5f1aac53483def28c7c26d9401197f8
Majid-Arab <arabmajid12@gmail.com>
2023-12-18 14:52:41 +0500

    created sidebar

--- js/myscript.js
+++ js/myscript.js
@@ -1,14 +1,19 @@
 // Function to open the modal
 function openModal() {
-    document.getElementById('myModal').style.display = 'block';
-    document.querySelector('.modal-overlay').style.display = 'block';
+  document.getElementById("myModal").style.display = "block";
+  document.querySelector(".modal-overlay").style.display = "block";
 }
 
 // Function to close the modal
 function closeModal() {
-    document.getElementById('myModal').style.display = 'none';
-    document.querySelector('.modal-overlay').style.display = 'none';
+  document.getElementById("myModal").style.display = "none";
+  document.querySelector(".modal-overlay").style.display = "none";
 }
 
 // Attach click event to the button
-document.getElementById('openModalBtn').addEventListener('click', openModal);
+document.getElementById("openModalBtn").addEventListener("click", openModal);
+
+function toggleMobileMenu() {
+  let mobileMenu = document.querySelector(".mobile-menu");
+  mobileMenu.classList.toggle("open");
+}
