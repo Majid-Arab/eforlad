67d00fe9021f345eac822766eab7375a5e27f363
Majid-Arab <arabmajid12@gmail.com>
2023-12-18 08:09:28 +0500

    created model

---
+++ js/myscript.js
@@ -0,0 +1,14 @@
+// Function to open the modal
+function openModal() {
+    document.getElementById('myModal').style.display = 'block';
+    document.querySelector('.modal-overlay').style.display = 'block';
+}
+
+// Function to close the modal
+function closeModal() {
+    document.getElementById('myModal').style.display = 'none';
+    document.querySelector('.modal-overlay').style.display = 'none';
+}
+
+// Attach click event to the button
+document.getElementById('openModalBtn').addEventListener('click', openModal);
